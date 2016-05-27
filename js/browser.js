49552066c1c0d53eba0ff8584b2e540fd104d683
Fernanda Lowe <fe_lowe@hotmail.com>
2016-05-27 08:02:34 -0700

    add folders and files to initiate project

---
+++ js/browser.js
@@ -0,0 +1,7 @@
+var wwww = require('../js/wwww.js').wwww;
+var yyyy = require('../js/yyyyy.js').yyyy;
+var xxxx = require('../js/xxxxx.js').xxxx;
+
+$(document).ready(function() {
+
+});
---
+++ js/yyyy.js
@@ -0,0 +1,3 @@
+exports.yyyy = function() {
+
+};

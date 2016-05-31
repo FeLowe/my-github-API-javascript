ff17227288dd9964a022490b82a35b8dfcbd49c6
Fernanda Lowe”git config --global user.name Fernanda <fe_lowe@hotmail.com>
2016-05-30 23:25:47 -0700

    add for loop so each object inside the array can be pull out

--- js/browser.js
+++ js/browser.js
@@ -2,12 +2,13 @@ var Repository = require('../js/repository.js').Repository;
 
 $(document).ready(function() {
   var currentRepository = new Repository();
-  $('#github-repo').click(function(){
+  $('#user-info').submit(function(event){
+    event.preventDefault();
     var user = $('#username').val();
     $('#username').val("");
     console.log(user);
     var repositories = currentRepository.getRepos(user);
     console.log(repositories);
-    $('.showRepositories').text('These are the repositories for:' + user + ':' + repositories + '.');
+    $('.showRepositories').text('These are the repositories for username: ' + user);
   });
 });
--- js/repository.js
+++ js/repository.js
@@ -2,10 +2,15 @@ var apiKey = require('./../.env').apiKey;
 
 exports.Repository = function(){
 };
+
 exports.Repository.prototype.getRepos = function(user) {
   $.get('https://api.github.com/users/'+ user +'/repos?access_token=' + apiKey).then(function(response){
-    console.log(response);
-    $('.showRepositories').text("These are the repositories for: " + user + ":" +response[0].name);
+console.log(response);
+
+    for (i = 0; i < response.length; i++){
+    $('.showRepositories').append('<li>' + 'Repository name: ' + response[i].name + '</li>' + '<a>'+ 'Repository url: ' + response[i].git_url + '</a>' + '<p>' + 'Repository description: ' + response[i].description + '</p>' +  '<p>' + 'Latest update time: ' + response[i].updated_at + '</p>');
+    }
+
   }).fail(function(error) {
     $('.showRepositories').text(error.responseJSON.message);
   });
